04e2ed4879ad49aecccf70995f222b14f46e90f9
thakurtejveer <thakurtejveer1@gmail.com>
2025-05-23 03:43:06 +0530

    added config package for AppConfig

--- src/main/java/com/scaler/productservicemay25/config/ApplicationConfig.java
+++ src/main/java/com/scaler/productservicemay25/config/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package com.scaler.productservicemay25;
+package com.scaler.productservicemay25.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
